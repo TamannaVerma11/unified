1c481fef47d734b176b7b89e999fe475a354c469
tamanna <tamanna@cye-in.local>
2023-03-27 12:55:18 +0530

    change folder

--- unified/settings.py
+++ unified/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myAdmin',
 ]
 
 ROOT_URLCONF = 'unified.urls'
