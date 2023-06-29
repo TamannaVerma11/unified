e415981119989bdbfc07e71f2bca9f8081a24db7
sumit <sumit.bhatia000@gmail.com>
2023-06-30 04:15:04 +0530

    fix bug

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -4,7 +4,7 @@ ssh root@128.199.25.122 <<EOF
   cd unified
   git pull
   source /opt/envs/unified/bin/activate
-  pip install -r requirements.txt
+  # pip install -r requirements.txt
   ./manage.py makemigrations
   ./manage.py migrate  --run-syncdb
   sudo service gunicorn restart
